531990b56d9bc517a78ce0f74bd7e79038c79f7e
Bilal-Raza7 <bilal.7897.raza@gmail.com>
2024-02-21 11:32:04 +0530

    some changes

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -17,10 +17,10 @@ interface User {
 }
 
 const pramdata = async ({ params }: { params: { id: string } }) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${params.id}`
-  );
-  const user: User[] = await res.json();
+  // const res = await fetch(
+  //   `https://jsonplaceholder.typicode.com/users/${params.id}`
+  // );
+  // const user: User[] = await res.json();
 
   return (
     <React.Fragment>
@@ -32,15 +32,21 @@ const pramdata = async ({ params }: { params: { id: string } }) => {
             className="lg:max-w-lg md:mx-w-md sm:mx-w-sm rounded-lg shadow-2xl"
           />
           <div>
-            <h1 className="text-5xl font-bold">{user.name}</h1>
+            <h1 className="text-5xl font-bold">Shoes</h1>
             {/* <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p> */}
-            <div className="flex justify-start py-6">
+            <div className="flex justify-start pt-6 pb-3">
               <div className="badge p-3 bg-success mx-4">call</div>
-              <p className="">{user.phone}</p>
+              <p className="">0987654321</p>
+            </div>
+            <div className="flex justify-start">
+              <div className="badge p-3 bg-secondary text-white mx-4 mb-2">
+                Price
+              </div>
+              <p className="font-bold text-xl">$ 124.99</p>
             </div>
             <p className="mx-3">Rate me Now</p>
             <div className="rating rating-lg my-3">
